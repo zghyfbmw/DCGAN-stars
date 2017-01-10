6fde71bbabf43052e90f606894e1cbbecdde8746
Kimi <276708965@qq.com>
2017-01-10 09:59:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from keras.layers.convolutional import Convolution2D, UpSampling2D
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
 from keras.layers.advanced_activations import LeakyReLU
+from keras.models import save_model, load_model
 
-X_train = np.load('/Users/zhangguanghua/Desktop/Stampede/T0.npy')
+X_train = np.load('/Users/zhangguanghua/Desktop/Stampede/T1.npy')
 X_train = (X_train - X_train.min()) / (X_train.max() - X_train.min())
 X_train = 1.0 - X_train
 print('X_train shape --- ', X_train.shape)
 print(X_train.shape[0], 'train samples')
-
 
 # generative model
 
@@ -39,7 +39,7 @@ generator.add(Activation('relu'))
 generator.add(Convolution2D(3, 5, 5, border_mode='same', dim_ordering='th'))
 generator.add(Activation('sigmoid'))
 
-generator.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.000006, beta_1=0.5))
+generator.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.000007, beta_1=0.5))
 generator.summary()
 
 # discriminative model
@@ -58,37 +58,40 @@ discriminator.add(LeakyReLU(0.2))
 discriminator.add(Dropout(0.5))
 discriminator.add(Dense(2, activation='softmax'))
 
-discriminator.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.000006, beta_1=0.5))
+discriminator.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.000007, beta_1=0.5))
 discriminator.summary()
 
-# GAN model
-
+# GAN Model
 gan_input = Input(shape=(100,))
 gan_output = discriminator(generator(gan_input))
 gan_model = Model(gan_input, gan_output)
 
-gan_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.000006, beta_1=0.5))
+gan_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.000007, beta_1=0.5))
 gan_model.summary()
 
+# Load Model
+generator.load_weights('/Users/zhangguanghua/Desktop/G_weights.h5')
+discriminator.load_weights('/Users/zhangguanghua/Desktop/D_weights.h5')
+
 print("Pre-training generator...")
-noise_gen = np.random.uniform(0, 1, size=(80000, 100))   # at (0,1) creates 10000 points
+noise_gen = np.random.uniform(0, 1, size=(100000, 100))   # at (0,1) creates 10000 points
 generated_images = generator.predict(noise_gen)
 
 print('generated_images shape ----', generated_images.shape)
 
-X = np.concatenate((X_train[:80000, :, :, :], generated_images))
-y = np.zeros([160000, 2])
-y[:80000, 1] = 1
-y[80000:, 0] = 1
+X = np.concatenate((X_train[:100000, :, :, :], generated_images))
+y = np.zeros([200000, 2])
+y[:100000, 1] = 1
+y[100000:, 0] = 1
 
-discriminator.fit(X, y, nb_epoch=1, batch_size=128)
+discriminator.fit(X, y, nb_epoch=1, batch_size=256)
 y_hat = discriminator.predict(X)
 
 # set up loss storage vector
 losses = {"d": [], "g": []}
 
 
-def train_for_n(nb_epoch=160000, batch_size=128):
+def train_for_n(nb_epoch=200000, batch_size=256):
     for e in range(nb_epoch):
 
         # Make generative images
@@ -96,7 +99,7 @@ def train_for_n(nb_epoch=160000, batch_size=128):
         mini_batch = X_train[train_idx]
         noise_gen = np.random.uniform(0, 1, size=(batch_size, 100))
         generated_images = generator.predict(noise_gen)
-        
+
         # Train discriminator on generated images
         X = np.concatenate((mini_batch, generated_images))
         y = np.zeros([2 * batch_size, 2])
@@ -120,12 +123,12 @@ def train_for_n(nb_epoch=160000, batch_size=128):
         losses["g"].append(g_loss)
 
         if e % 100 == 0:
-            generator.save_weights('G_weights.h5')
-            discriminator.save_weights('D_weights.h5')
+            generator.save_weights('G1_weights.h5')
+            discriminator.save_weights('D1_weights.h5')
             noise = np.random.uniform(0, 1, size=(100, 100))
             generated_images = generator.predict(noise)
             np.save('/Users/zhangguanghua/Desktop/Stampede/generated_images.npy', generated_images)
 
         print("Iteration: {0} / {1}, Loss: {2:.4f}".format(e, nb_epoch, float(g_loss)))
 
-train_for_n(nb_epoch=2000, batch_size=128)
+train_for_n(nb_epoch=2000, batch_size=256)
